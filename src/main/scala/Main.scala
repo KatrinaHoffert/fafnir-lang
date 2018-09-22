f0d0c4e084493b8a2d47d599e77bb986d2907d43
Katrina Mitchell <kat.hoffert@gmail.com>
2018-09-21 22:30:46 -0600

    Handling terms

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,37 +4,52 @@ case class Identifier(name: String) {
   override def toString: String = name
 }
 
-case class IntExpression(x: Int) extends Expression {
+case class IntEvaluable(x: Int) extends Term {
   override def evaluate: Int = x
   override def toString: String = s"$x"
 }
 
-case class AdditionExpression(x: Expression, y: Expression) extends Expression {
+case class AdditionEvaluable(x: Evaluable, y: Evaluable) extends Expression {
   override def evaluate: Int = x.evaluate + y.evaluate
   override def toString: String = s"$x + $y"
 }
 
-case class SubtractionExpression(x: Expression, y: Expression) extends Expression {
+case class SubtractionEvaluable(x: Evaluable, y: Evaluable) extends Expression {
   override def evaluate: Int = x.evaluate - y.evaluate
   override def toString: String = s"$x - $y"
 }
 
-abstract class Expression {
+case class MultiplicationTerm(x: Evaluable, y: Evaluable) extends Term {
+  override def evaluate: Int = x.evaluate * y.evaluate
+  override def toString: String = s"($x * $y)"
+}
+
+case class DivisionTerm(x: Evaluable, y: Evaluable) extends Term {
+  override def evaluate: Int = x.evaluate / y.evaluate
+  override def toString: String = s"($x / $y)"
+}
+
+abstract class Evaluable {
   def evaluate: Int
 }
+abstract class Expression extends Evaluable
+abstract class Term extends Expression
 
 class SimpleParser extends RegexParsers {
   def identifier: Parser[Identifier] = """[A-Za-z_][A-Za-z_0-9]+""".r ^^ { name => Identifier(name) }
-  def expression: Parser[Expression] = addition | subtraction | int
-  def int: Parser[Expression] = """[0-9]+""".r ^^ { x => IntExpression(x.toInt) }
-  def addition: Parser[Expression] = int ~ "+" ~ expression ^^ { case x ~ _ ~ y => AdditionExpression(x, y) }
-  def subtraction: Parser[Expression] = int ~ "-" ~ expression ^^ { case x ~ _ ~ y => SubtractionExpression(x, y) }
+  def term: Parser[Term] = multiplication | division | int
+  def expression: Parser[Expression] = addition | subtraction | term
+  def int: Parser[Term] = """[0-9]+""".r ^^ { x => IntEvaluable(x.toInt) }
+  def multiplication: Parser[Term] = int ~ "*" ~ term ^^ { case x ~ _ ~ y => MultiplicationTerm(x, y) }
+  def division: Parser[Term] = int ~ "/" ~ term ^^ { case x ~ _ ~ y => DivisionTerm(x, y) }
+  def addition: Parser[Expression] = int ~ "+" ~ expression ^^ { case x ~ _ ~ y => AdditionEvaluable(x, y) }
+  def subtraction: Parser[Expression] = int ~ "-" ~ expression ^^ { case x ~ _ ~ y => SubtractionEvaluable(x, y) }
 }
 
 object TestSimpleParser extends SimpleParser {
   def main(args: Array[String]): Unit = {
-    parse(expression, "4 + 3 - 1") match {
-      case Success(matched, _) => println(matched.evaluate)
+    parse(expression, "4 + 3 - 1 * 2") match {
+      case Success(matched, _) => println(matched.toString)
       case Failure(msg, _) => println(s"FAILURE: $msg")
       case Error(msg, _) => println(s"ERROR: $msg")
     }
