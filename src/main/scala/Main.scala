8d4bf80f74a31a23ee948eec9a54ed026f8c57a4
Katrina Mitchell <kat.hoffert@gmail.com>
2018-09-22 12:39:14 -0600

    Utterly rudimentary type system start
    Now have string literals and values can have any arbitrary type.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,38 +4,112 @@ case class Identifier(name: String) {
   override def toString: String = name
 }
 
-case class IntEvaluable(x: Int) extends Primary {
-  override def evaluate: Int = x
+case class StringLiteral(x: String) extends Primary {
+  override def evaluate: ValueInstance = StringValue(x)
+  override def toString: String = s""""$x""""
+}
+
+case class IntLiteral(x: Int) extends Primary {
+  override def evaluate: ValueInstance = IntValue(x)
   override def toString: String = s"$x"
 }
 
 case class AdditionEvaluable(x: Evaluable, y: Evaluable) extends Expression {
-  override def evaluate: Int = x.evaluate + y.evaluate
+  override def evaluate: ValueInstance = x.evaluate + y.evaluate
   override def toString: String = s"$x + $y"
 }
 
 case class SubtractionEvaluable(x: Evaluable, y: Evaluable) extends Expression {
-  override def evaluate: Int = x.evaluate - y.evaluate
+  override def evaluate: ValueInstance = x.evaluate - y.evaluate
   override def toString: String = s"$x - $y"
 }
 
 case class Braces(expression: Expression) extends Primary {
-  override def evaluate: Int = expression.evaluate
+  override def evaluate: ValueInstance = expression.evaluate
   override def toString: String = s"($expression)"
 }
 
 case class MultiplicationTerm(x: Evaluable, y: Evaluable) extends Term {
-  override def evaluate: Int = x.evaluate * y.evaluate
+  override def evaluate: ValueInstance = x.evaluate * y.evaluate
   override def toString: String = s"($x * $y)"
 }
 
 case class DivisionTerm(x: Evaluable, y: Evaluable) extends Term {
-  override def evaluate: Int = x.evaluate / y.evaluate
+  override def evaluate: ValueInstance = x.evaluate / y.evaluate
   override def toString: String = s"($x / $y)"
 }
 
+object BaseTypes extends Enumeration {
+  val integer, string = Value
+}
+
+abstract class ValueInstance {
+  def +(that: ValueInstance): ValueInstance
+  def -(that: ValueInstance): ValueInstance
+  def *(that: ValueInstance): ValueInstance
+  def /(that: ValueInstance): ValueInstance
+}
+
+case class IntValue(value: Int) extends ValueInstance {
+  def +(that: ValueInstance): ValueInstance = {
+    that match {
+      case intThat: IntValue => IntValue(this.value + intThat.value)
+      case _ => throw new Exception("Not supported type")
+    }
+  }
+
+  def -(that: ValueInstance): ValueInstance = {
+    that match {
+      case intThat: IntValue => IntValue(this.value - intThat.value)
+      case _ => throw new Exception("Not supported type")
+    }
+  }
+
+  def *(that: ValueInstance): ValueInstance = {
+    that match {
+      case intThat: IntValue => IntValue(this.value * intThat.value)
+      case _ => throw new Exception("Not supported type")
+    }
+  }
+
+  def /(that: ValueInstance): ValueInstance = {
+    that match {
+      case intThat: IntValue => IntValue(this.value / intThat.value)
+      case _ => throw new Exception("Not supported type")
+    }
+  }
+
+  override def toString: String = this.value.toString
+}
+
+case class StringValue(value: String) extends ValueInstance {
+  def +(that: ValueInstance): ValueInstance = {
+    that match {
+      case stringThat: StringValue => StringValue(this.value + stringThat.value)
+      case _ => throw new Exception("Not supported type")
+    }
+  }
+
+  def -(that: ValueInstance): ValueInstance = {
+    throw new Exception("Not supported type")
+  }
+
+  def *(that: ValueInstance): ValueInstance = {
+    that match {
+      case intThat: IntValue => StringValue(this.value * intThat.value)
+      case _ => throw new Exception("Not supported type")
+    }
+  }
+
+  def /(that: ValueInstance): ValueInstance = {
+    throw new Exception("Not supported type")
+  }
+
+  override def toString: String = s""""${this.value}""""
+}
+
 abstract class Evaluable {
-  def evaluate: Int
+  def evaluate: ValueInstance
 }
 abstract class Expression extends Evaluable
 abstract class Term extends Expression
@@ -43,10 +117,14 @@ abstract class Primary extends Term
 
 class SimpleParser extends RegexParsers {
   def identifier: Parser[Identifier] = """[A-Za-z_][A-Za-z_0-9]+""".r ^^ { name => Identifier(name) }
-  def primary: Parser[Primary] = braces | int
+  def intLiteral: Parser[Primary] = """[0-9]+""".r ^^ { x => IntLiteral(x.toInt) }
+  def stringLiteral: Parser[Primary] = """"[^"\\]*(\\.[^"\\]*)*"""".r ^^ { x =>
+    val stringContents = x.slice(1, x.length - 1)
+    StringLiteral(stringContents)
+  }
+  def primary: Parser[Primary] = braces | intLiteral | stringLiteral
   def term: Parser[Term] = multiplication | division | primary
   def expression: Parser[Expression] = addition | subtraction | term
-  def int: Parser[Primary] = """[0-9]+""".r ^^ { x => IntEvaluable(x.toInt) }
   def braces: Parser[Primary] = "(" ~ expression ~ ")" ^^ { case _ ~ x ~ _ => Braces(x)}
   def multiplication: Parser[Term] = primary ~ "*" ~ term ^^ { case x ~ _ ~ y => MultiplicationTerm(x, y) }
   def division: Parser[Term] = primary ~ "/" ~ term ^^ { case x ~ _ ~ y => DivisionTerm(x, y) }
@@ -56,10 +134,19 @@ class SimpleParser extends RegexParsers {
 
 object TestSimpleParser extends SimpleParser {
   def main(args: Array[String]): Unit = {
-    parse(expression, "4 + (3 - 1) * 2") match {
-      case Success(matched, _) => println(matched.toString)
-      case Failure(msg, _) => println(s"FAILURE: $msg")
-      case Error(msg, _) => println(s"ERROR: $msg")
+    val expressions = Seq(
+      "4 + (3 - 1) * 2",
+      "\"hello\" * 2",
+      "\"hello\" + \" \" + \"world\"",
+      "5 / 2 - 2",
+    )
+
+    for(expr <- expressions) {
+      parse(expression, expr) match {
+        case Success(matched, _) => println(s"$expr = ${matched.evaluate}")
+        case Failure(msg, _) => println(s"FAILURE: $msg")
+        case Error(msg, _) => println(s"ERROR: $msg")
+      }
     }
   }
 }
