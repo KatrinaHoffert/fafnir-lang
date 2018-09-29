c51095c91fc718284f876b1649f19decc9dc55ae
Katrina Mitchell <kat.hoffert@gmail.com>
2018-09-28 18:03:12 -0600

    Reorganized tests for cleaner multi-line strings

--- src/test/scala/ProgramTest.scala
+++ src/test/scala/ProgramTest.scala
@@ -4,25 +4,29 @@ class ProgramTest extends FunSuite {
   test("Programs produce the correct final state") {
     val parser = new FafnirParser()
 
-    val program = """
-      var x = 4 + (3 - 1) * 2;
-      if(x)
-      {
-        var y = 2;
-        x = x + y;
-      }
-      if(x - 10) {
-        var notReached = "foo";
-      }
-      var z = "Something new";
-      {
-        var local = "Will be out of scope at end of program";
-        z = z + " or old";
-      }
-    """
+    val program =
+      """
+        |var x = 4 + (3 - 1) * 2;
+        |if(x)
+        |{
+        |  var y = 2;
+        |  x = x + y;
+        |}
+        |
+        |if(x - 10) {
+        |  var notReached = "foo";
+        |}
+        |else { x = x + 1; }
+        |
+        |var z = "Something new";
+        |{
+        |  var local = "Will be out of scope at end of program";
+        |  z = z + " or old";
+        |}
+      """.stripMargin
 
     val expectedVariables = Map(
-      "x" -> IntValue(10),
+      "x" -> IntValue(11),
       "z" -> StringValue("Something new or old"),
     )
 
@@ -42,60 +46,60 @@ class ProgramTest extends FunSuite {
     // in it having a different value.
     val programs = Seq(
       """
-        if(1) { a = 1; }
-        elif(1 + 2) { a = 123; }
-        elif(0 / 0) { a = 123; }
-        else { a = 123; }
-      """,
+        |if(1) { a = 1; }
+        |elif(1 + 2) { a = 123; }
+        |elif(0 / 0) { a = 123; }
+        |else { a = 123; }
+      """.stripMargin,
       """
-        if (a) {
-          a = 123;
-        } elif (0) {
-          a = 123;
-        } elif (a + 1) {
-          a = 1;
-        }
-      """,
+        |if (a) {
+        |  a = 123;
+        |} elif (0) {
+        |  a = 123;
+        |} elif (a + 1) {
+        |  a = 1;
+        |}
+      """.stripMargin,
       """
-        if(0){a=123;}
-        elif(0){a=123;}
-        else{a=1;}
-      """,
+        |if(0){a=123;}
+        |elif(0){a=123;}
+        |else{a=1;}
+      """.stripMargin,
       """
-        if (0) {
-          a = 123;
-        } elif (0) {
-          a = 123;
-        } elif (0) {
-          a = 123;
-        }
-        else {
-          a = 1;
-        }
-      """,
+        |if (0) {
+        |  a = 123;
+        |} elif (0) {
+        |  a = 123;
+        |} elif (0) {
+        |  a = 123;
+        |}
+        |else {
+        |  a = 1;
+        |}
+      """.stripMargin,
       """
-        if (1) {
-          a = 1;
-        }
-      """,
+        |if (1) {
+        |  a = 1;
+        |}
+      """.stripMargin,
       """
-        if (0) {
-          a = 123;
-        }
-        a = a + 1;
-      """,
+        |if (0) {
+        |  a = 123;
+        |}
+        |a = a + 1;
+      """.stripMargin,
       """
-        if (1) {
-          if(0)
-          {
-            a = 123;
-          }
-          else
-          {
-            if(1){ a = 1; }
-          }
-        }
-      """
+        |if (1) {
+        |  if(0)
+        |  {
+        |    a = 123;
+        |  }
+        |  else
+        |  {
+        |    if(1){ a = 1; }
+        |  }
+        |}
+      """.stripMargin
     )
 
     for(p <- programs) {
@@ -113,57 +117,55 @@ class ProgramTest extends FunSuite {
   test("Statements pretty print") {
     val parser = new FafnirParser()
 
-    val program = """
-      var x = 4 + (3 - 1) * 2;
-      {var y = 9;}
-      y = 10;
+    val program =
+      """
+        |var x = 4 + (3 - 1) * 2;
+        |{var y = 9;}
+        |y = 10;
+        |if (1) {
+        |  if(0)
+        |  {
+        |    y = 123;
+        |  }
+        |  elif ( x + y) {
+        |    y = 123;
+        |  }
+        |  elif (x - y) {
+        |    y = 321; }
+        |  else
+        |  {
+        |    if(1){ a = 1; }
+        |  }
+        |}
+      """.stripMargin
 
-      if (1) {
-        if(0)
-        {
-          y = 123;
-        }
-        elif ( x + y) {
-          y = 123;
-        }
-        elif (x - y) {
-          y = 321; }
-        else
-        {
-          if(1){ a = 1; }
-        }
-      }
-    """
-
-    val expectedOutput = """
-var x = 4 + ((3 - 1) * 2);
-{
-  var y = 9;
-}
-y = 10;
-if(1) {
-  if(0) {
-    y = 123;
-  }
-  elif(x + y) {
-    y = 123;
-  }
-  elif(x - y) {
-    y = 321;
-  }
-  else {
-    if(1) {
-      a = 1;
-    }
-  }
-}
-    """.trim
+    val expectedOutput =
+      """
+        |var x = 4 + ((3 - 1) * 2);
+        |{
+        |  var y = 9;
+        |}
+        |y = 10;
+        |if(1) {
+        |  if(0) {
+        |    y = 123;
+        |  }
+        |  elif(x + y) {
+        |    y = 123;
+        |  }
+        |  elif(x - y) {
+        |    y = 321;
+        |  }
+        |  else {
+        |    if(1) {
+        |      a = 1;
+        |    }
+        |  }
+        |}
+      """.stripMargin.trim
 
     parser.parse(parser.program, program) match {
-      case parser.Success(matched, _) =>
-        println(matched.toString)
-        println(expectedOutput)
-        assert(matched.toString === expectedOutput)
+      case parser.Success(matched, _) => assert(matched.toString === expectedOutput)
       case parser.Failure(msg, _) => fail(s"Parse failure: $msg")
       case parser.Error(msg, _) => fail(s"Parse error: $msg")
     }
